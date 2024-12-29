fbc0a036592b2fc2f9a8e8b8972d46fc34b86ae9
Samoilenko S. <fb.alferova.s@gmail.com>
2024-12-30 00:31:42 +0200

    finally

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,34 +1,58 @@
-import { useState } from "react";
-import reactLogo from "../assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useState, useEffect } from "react";
+import ContactList from "./ContactList/ContactList";
+import ContactForm from "./ContactForm/ContactForm";
+import SearchBox from "./SearchBox/SearchBox";
+import styles from "/src/App.module.css";
 
-function App() {
-  const [count, setCount] = useState(0);
+const App = () => {
+  const [contacts, setContacts] = useState(() => {
+    const savedContacts = JSON.parse(localStorage.getItem("contacts"));
+    if (savedContacts?.length) {
+      return savedContacts;
+    }
+    return [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+  }, [contacts]);
+
+  const deleteContact = (id) => {
+    setContacts((prev) => prev.filter((item) => item.id !== id));
+  };
+
+  const [newValue, setNewValue] = useState("");
+
+  const filteredContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(newValue.toLowerCase())
+  );
+
+  const handleFilter = (e) => setNewValue(e.target.value);
+
+  const addContact = (newContact) => {
+    const isDuplicate = contacts.some(
+      (contact) => contact.name.toLowerCase() === newContact.name.toLowerCase()
+    );
+
+    if (isDuplicate) {
+      alert(`${newContact.name} is already in contacts`);
+      return;
+    }
+
+    setContacts((prev) => [...prev, newContact]);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className={styles.wrapper}>
+      <h1 className={styles.title}>Phonebook</h1>
+      <ContactForm onAddContact={addContact} />
+      <SearchBox newValue={newValue} onChange={handleFilter} />
+      <ContactList
+        contacts={filteredContacts}
+        onDeleteContact={deleteContact}
+      />
+    </div>
   );
-}
+};
 
 export default App;
